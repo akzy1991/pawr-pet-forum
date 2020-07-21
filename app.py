5c2ada430a332507e37faf97530ad0cc42a2f954
akzy1991 <richo_koh@hotmail.com>
2020-07-21 12:32:14 +0000

    implemented user login with flask login

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import pymongo
+import flask_login
 from dotenv import load_dotenv
 from forms import NewPostForm, RegistrationForm, LoginForm
 from bson import ObjectId
@@ -14,6 +15,32 @@ app = Flask(__name__)
 # connect to mongo
 MONGO_URI = os.environ.get('MONGO_URI')
 client = pymongo.MongoClient(MONGO_URI)
+
+# login manager
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+
+# create user object
+class User(flask_login.UserMixin):
+    pass
+
+
+# user loader
+@login_manager.user_loader
+def user_loader(email):
+    # find the user by email
+    user_in_db = client[DB_NAME].users.find_one({
+        'email': email
+    })
+    # create user object
+    user = User()
+
+    if user_in_db:
+        user.id = user_in_db['email']
+    else:
+        return None
+
 
 # define my db_name
 DB_NAME = "pawr"
@@ -37,7 +64,12 @@ def home():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash("Account Created", "success")
+        client[DB_NAME].users.insert_one({
+            'username': form.username.data,
+            'email': form.email.data,
+            'password': form.password.data
+        })
+        flash('Account Created Succesfully', "success")
         return redirect(url_for('home'))
     return render_template('register.template.html',
                            form=form, title='Sign Up')
@@ -47,26 +79,33 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'dummy_email@gemail.com' and form.password.data == 'Password123':
-            flash("Successfully Logged In", "success")
-            return redirect(url_for('home'))
+        user_in_db = client[DB_NAME].users.find_one({
+            'email': form.email.data
+        })
+        if user_in_db:
+            user = User()
+            user.id = user_in_db['email']
+            if form.password.data == user_in_db['password']:
+                flask_login.login_user(user)
+                flash("Successfully Logged In", "success")
+                return redirect(url_for('home'))
+            else:
+                flash('Login failed, please check username and password',
+                      'danger')
         else:
-            flash('Login failed, please check username and password', 'danger')
+            flash('User not found', 'danger')
     return render_template('login.template.html',
                            form=form, title='Login')
 
 
-@app.route('/post/create', methods=["GET", "POST"])
+@app.route('/post/new', methods=["GET", "POST"])
 def new_post():
     form = NewPostForm()
     if form.validate_on_submit():
-        title = form.title.data
-        content = form.content.data
-        date_time = datetime.now().replace(microsecond=0)
         client[DB_NAME].posts.insert_one({
-            'title': title,
-            'content': content,
-            'datetime_posted': date_time
+            'title': form.title.data,
+            'content': form.content.data,
+            'datetime_posted': datetime.now().replace(microsecond=0)
         })
         flash(f"New post '{form.title.data}' created", "success")
         return redirect(url_for('home'))
